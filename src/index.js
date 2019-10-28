33aac5d65debee8f0558d7852916c0346f6fa313
Shorn Jacob <shorn@transparency.consulting>
2019-10-29 12:40:51 +1300

    interfaceroboto

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+import { Helmet } from "react-helmet";
+import 'typeface-roboto';
+import Types from './pages/Types'
 
 function App() {
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <div>
+      <Helmet>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+      </Helmet>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+
+      <Types />
+    </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
+//https://material-ui.com/getting-started/usage/#responsive-meta-tag
+//Responsibe meta tag
